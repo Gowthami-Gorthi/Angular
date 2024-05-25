73b9f803126ec27bc10c4a30b0b366e13817aeea
Gowthami <gorthigowthami8@gmail.com>
2024-05-25 13:51:29 +0530

    Peroject created

---
+++ AngularProject/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'AngularProject';
+  
+  Call(val:any){
+    console.log(val);
+  }
+}
